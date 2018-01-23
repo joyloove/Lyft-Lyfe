0e1df25a3a24c0e09c8ac8342d2c9450eeb3be28
joyloove <jocelyn.esquivel24@gmail.com>
2018-01-22 19:38:22 -0600

    Actualizando botones

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   setTimeout(function() {
     $('#logo').fadeOut(500);
     $('#first').show();
-  }, 2000);
+  }, 2000); //splash
 
 
   $('select').material_select();
@@ -16,7 +16,7 @@ $(document).ready(function() {
     if ($(this).val().length === 10) {
       $btnNext.attr({
         disabled: false,
-        href: '../views/verify.html'
+        href: '../views/verify.html' //verify the phone number have 10 digits
       });
     } else {
       $btnNext.attr('disabled', 'disabled');
@@ -24,7 +24,7 @@ $(document).ready(function() {
   });
 
   $btnNext.click(function() {
-    var number = Math.floor(Math.random() * 900) + 100;
+    var number = Math.floor(Math.random() * 800) + 100; //generate the code and send to user in alert
     alert('LAB - ' + number);
     localStorage.labCode = number;
   });
@@ -35,13 +35,13 @@ $(document).ready(function() {
 
   $inputPhone.keypress(function(tecla) {
     if (tecla.charCode < 48 || tecla.charCode > 57)
-      return false;
+      return false; //verify the code
   });
 
   $inputCode.eq(0).focus();
   $inputCode.keyup(function() {
     var code1 = $inputCode.eq(0).val(),
-      code2 = $inputCode.eq(1).val(),
+      code2 = $inputCode.eq(1).val(), //compare and return the index
       code3 = $inputCode.eq(2).val();
 
     var code = code1 + code2 + code3;
@@ -49,7 +49,7 @@ $(document).ready(function() {
     if (code === localStorage.labCode) {
       $btnCheck.attr({
         disabled: false,
-        href: '../views/form.html'
+        href: '../views/form.html' //verify the code and redirect to the next
       });
     } else {
       $btnCheck.attr('disabled', true);
@@ -58,8 +58,8 @@ $(document).ready(function() {
 
   $btnResend.click(function() {
     $inputCode.val('');
-    var number = Math.floor(Math.random() * 900) + 100;
-    alert('LAB - ' + number);
+    var number = Math.floor(Math.random() * 800) + 100;
+    alert('LAB - ' + number); //resend the code
     localStorage.labCode = number;
   });
 
@@ -68,9 +68,9 @@ $(document).ready(function() {
   var validateChecked = false;
 
   $('#inputEmail').on('input', function(event) {
-    var REGEXEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    if (REGEXEMAIL.test($(this).val())) {
-      validateEmail = true;
+    var CHECKEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    if (CHECKEMAIL.test($(this).val())) {
+      validateEmail = true; //validate the email
       activeButton();
     } else {
       desactiveButton();
@@ -82,7 +82,7 @@ $(document).ready(function() {
       validateName = true;
       activeButton();
     } else {
-      desactiveButton();
+      desactiveButton(); //validate the name field
     }
   });
 
@@ -90,7 +90,7 @@ $(document).ready(function() {
     if (event.target.checked) {
       validateChecked = true;
       activeButton();
-    } else {
+    } else {  //if the checkbox is check button next appear able to click
       desactiveButton();
     }
   });
@@ -99,7 +99,7 @@ $(document).ready(function() {
     if (validateName && validateEmail && validateChecked) {
       $('#submit').attr({
         disabled: false,
-        href: '../views/success.html'
+        href: '../views/success.html' // when the user complete your register redirection to successfully view
       });
     }
   }
